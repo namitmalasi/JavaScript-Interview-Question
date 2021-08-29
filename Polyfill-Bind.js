1741791635c222196f4588b0d44c65f12ba0267d
Namit Malasi <namitmalasi@gmail.com>
2021-08-29 20:58:13 +0530

    added the polyfill-bind implementation

---
+++ Polyfill-Bind.js
@@ -0,0 +1,31 @@
+// Implement the polyfill for the Bind() function in JavaScript.
+
+const name1 = {
+  firstname: "AJ",
+  lastname: "Styles",
+};
+
+function printname(hometown, state, country) {
+  console.log(
+    this.firstname +
+      " " +
+      this.lastname +
+      " " +
+      hometown +
+      " " +
+      state +
+      " " +
+      country
+  );
+}
+
+Function.prototype.myBind = function (...args) {
+  let obj = this;
+  let params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [...params, ...args2]);
+  };
+};
+
+let print = printname.myBind(name1, "Mumbai", "Maharashtra");
+print("india");
