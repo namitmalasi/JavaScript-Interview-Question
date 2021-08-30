5563ba68f2b9ad29bdbf6c311c26cb5af5709fc8
Namit Malasi <namitmalasi@gmail.com>
2021-08-30 19:01:04 +0530

    added the implementation of polyfill for Map function in javaScript

---
+++ Polyfill-map.js
@@ -0,0 +1,19 @@
+// Implement the polyfill for the Map() function in JavaScript.
+
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+Array.prototype.myMap = function (fn) {
+  newArray = [];
+  let obj = this;
+  for (let index = 0; index < obj.length; index++) {
+    newArray.push(fn(obj[index]));
+  }
+  return newArray;
+};
+
+const output = array.myMap((item) => {
+  return item;
+});
+
+console.log(output);
