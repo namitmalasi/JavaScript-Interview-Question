0c804f9234c531039f32681d9d52cf7fab2ee27b
Namit Malasi <namitmalasi@gmail.com>
2021-09-20 20:26:13 +0530

    implementation of First non repeating character

---
+++ FirstNonRepeatingCharacter.js
@@ -0,0 +1,21 @@
+const firstNonRepeatingCharacter = (str) => {
+  let obj = {};
+
+  for (let item of str) {
+    if (obj[item]) {
+      obj[item]++;
+    } else {
+      obj[item] = 1;
+    }
+  }
+
+  for (let item in obj) {
+    if (obj[item] === 1) {
+      return str.indexOf(item);
+    }
+  }
+  return -1;
+};
+
+console.log(firstNonRepeatingCharacter("namit"));
+console.log(firstNonRepeatingCharacter("aabbccdd"));
