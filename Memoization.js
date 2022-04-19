18fd9a1d2913f6a0bfad20d67152a0dfb9f052dc
Namit Malasi <namitmalasi@gmail.com>
2022-04-19 12:52:47 +0530

    Memoization

---
+++ Memoization.js
@@ -0,0 +1,31 @@
+function memoize(fn) {
+  const cache = {};
+  return (...args) => {
+    const arg = JSON.stringify(args);
+
+    if (arg in cache) {
+      console.log("fetching from cache");
+      return cache[arg];
+    } else {
+      console.log("fetching for the first time");
+      const result = fn.apply(this, args);
+      cache[arg] = result;
+      return result;
+    }
+  };
+}
+
+const addThreeNums = (a, b, c) => a + b + c;
+
+const add = memoize(addThreeNums);
+console.time();
+console.log(add(1, 2, 3));
+console.timeEnd();
+
+console.time();
+console.log(add(1, 2, 3));
+console.timeEnd();
+
+console.time();
+console.log(add(1, 2, 3));
+console.timeEnd();
